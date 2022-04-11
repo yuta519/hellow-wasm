840e614c4a48a471fdc7f43bb76ba000b901d64c
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-12 08:55:29 +0900

    add Readme for later reading

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 > cargo new --lib hello-wasm
 
 Before executing, you have to install wasm-pack,
+
+
+## Compile from Rust to WASM
+> wasm-pack build --target web
+
+After above command, you can see the directory `pkg` is created.
+
+## Check wasm running
+> python3 -m http.server
+
+Run above command and visit https://localhost:8000
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use wasm_bindgen::prelude::*;
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
